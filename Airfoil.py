c94470677f1c78bebd46598168b29b9d9296a287
Swarna Dutta <124623157+Swarna0017@users.noreply.github.com>
2024-08-15 00:41:49 +0530

    Update Airfoil.py

--- Airfoil.py
+++ Airfoil.py
@@ -6,12 +6,16 @@ class Airfoil:
         self.cm_data = cm_data
 
 # Addition of Some code to fetch airfoil data. Fetch cl, cd and cm of an airfoil
+# say sample data is # Sample data
+# cl_data = {'alpha': [0, 5, 10, 15], 'cl': [0.0, 0.5, 1.0, 0.8]}
+# cd_data = {'alpha': [0, 5, 10, 15], 'cd': [0.01, 0.015, 0.02, 0.025]}
+# cm_data = {'alpha': [0, 5, 10, 15], 'cm': [0.0, -0.02, -0.04, -0.03]}
 
-    # def get_cl(self, aoa: float):
-    #     return np.interp(aoa, self.cl_data['alpha'], self.cl_data['cl'])
+    def get_cl(self, aoa: float):
+        return np.interp(aoa, self.cl_data['alpha'], self.cl_data['cl'])
     
-    # def get_cd(self, aoa: float):
-    #     return np.interp(aoa, self.cd_data['alpha'], self.cd_data['cd'])
+    def get_cd(self, aoa: float):
+        return np.interp(aoa, self.cd_data['alpha'], self.cd_data['cd'])
 
-    # def get_cm(self, aoa: float):
-    #     return np.interp(aoa, self.cm_data['alpha'], self.cm_data['cd'])
+    def get_cm(self, aoa: float):
+        return np.interp(aoa, self.cm_data['alpha'], self.cm_data['cd'])

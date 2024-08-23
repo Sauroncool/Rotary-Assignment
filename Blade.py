d25c14244086965779a736546b3dfc68ef162a70
Swarna Dutta <124623157+Swarna0017@users.noreply.github.com>
2024-08-24 01:03:29 +0530

    Update Blade.py

--- Blade.py
+++ Blade.py
@@ -1,2 +1,7 @@
 #Fetches input from the user input file and specifies blade geometry and parameters as per the instructions.
-import get_pilot_inputs from FS_user_inputs
+from FS_user_inputs import get_pilot_inputs 
+from FS_user_inputs import get_user_inputs
+
+MR_cyclic_pitch = theta      #Defining theta here to import in the II and CI for later.
+
+# ADD MORE ROTOR SPECIFIC STUFF

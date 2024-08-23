5c60d0846ef122b81fbc38d2345b2cfc55919e94
Swarna Dutta <124623157+Swarna0017@users.noreply.github.com>
2024-08-24 01:00:39 +0530

    Update FS_user_inputs.py

--- FS_user_inputs.py
+++ FS_user_inputs.py
@@ -4,3 +4,10 @@
 def get_user_inputs():
     inputs = {}
     # Input parameter stuff
+
+def get_pilot_inputs():
+    MR_collective      =float(input("Main Rotor Collective pitch (Degrees): "))        # Altitude control
+    MR_cyclic_pitch    =float(input("Main Rotor Cyclic pitch (Degrees): "))            # Helicopter Pitch control/ theta
+    MR_cyclic_roll     =float(input("Main Rotor Cyclic Roll (Degrees): "))             # Helicopter Roll/sideway tilt
+    TR_collective      =float(input("Tail Rotor Collective pitch (Degrees): "))        # Yaw control
+    return MR_collective, MR_cyclic_pitch, MR_cyclic_roll, TR_collective

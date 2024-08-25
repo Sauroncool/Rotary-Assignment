0ae608a646b927d14731b74b47dfd54d453c84e8
MayankKr007 <scimayank@gmail.com>
2024-08-25 12:26:37 +0530

    Update Blade.py
    added complete blade profiling for both main and tail rotors

--- Blade.py
+++ Blade.py
@@ -5,3 +5,22 @@ from FS_user_inputs import get_user_inputs
 MR_cyclic_pitch = theta      #Defining theta here to import in the II and CI for later.
 
 # ADD MORE ROTOR SPECIFIC STUFF
+#MK defined
+
+MR_radius       = user_inputs(main_radius)      # main rotor blade radius, in metres
+MR_nu_blades    = user_inputs(main_number)      # number of blades in main rotor
+MR_omega        = user_inputs(main_omega)  # main rotor rotation rate, in rpm
+MR_root_cutout  = user_inputs(main_root_cutout) # main rotor root cutout, in metres
+MR_taper        = user_inputs(main_taper)    # main rotor taper, defined as taper = (chord_tip - chord_root) / (R_tip - R_root)
+MR_twist        = user_inputs(main_twist)    # main rotor twist, defined as twist = (theta_tip - theta_root) / (R_tip - R_root)
+MR_collective   = pilot_inputs(main_collective) # main rotor collective theta input by pilot
+MR_cyclic_a1    = pilot_inputs(main_cyclic_a1)  # main rotor cyclic theta - a1 input
+MR_cyclic_a2    = pilot_inputs(main_cyclic_a2)  # main rotor cyclic theta - a2 input
+
+
+TR_radius       = user_inputs(tail_radius)      # tail rotor blade radius, in metres
+TR_nu_blades    = user_inputs(tail_number)      # number of blades in tail rotor
+TR_omega        = user_inputs(tail_omega)  # tail rotor rotation rate, in rpm
+TR_root_cutout  = user_inputs(tail_root_cutout) # tail rotor root cutout, in metres
+TR_taper        = user_inputs(tail_taper)    # tail rotor taper, defined as taper = (chord_tip - chord_root) / (R_tip - R_root)
+TR_twist        = user_inputs(tail_twist)    # tail rotor twist, defined as twist = (theta_tip - theta_root) / (R_tip - R_root)

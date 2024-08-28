47bae71dcbc97e41231ba8d1fdc31032c1e12711
Sauroncool <125077528+Sauroncool@users.noreply.github.com>
2024-08-28 17:49:02 +0530

    Add files via upload

---
+++ mission planner/mission-planner.py
@@ -0,0 +1,8 @@
+from mission_inputs import *
+
+Thrust = 2000 # N
+def fuel_burn_rate:
+    m_dot = 0.03*Thrust
+    return m_dot
+
+def fuel_weight
---
+++ mission planner/mission_inputs.py
@@ -0,0 +1,21 @@
+tk_off_weight = 500 # kg
+tk_off_attitude = 400 # m
+fuel_weight = 500 # kg
+
+#Hover and vertical climb
+# put params here
+
+
+
+# forward and steady climb
+
+
+
+
+# payload drop
+
+
+
+
+
+# pickup

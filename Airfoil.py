fdc1326e435cde67f1a45d021c9348cef36cb9c6
Your Name <Swarna Dutta>
2024-08-28 14:57:17 +0530

    changing the a/f function

--- Airfoil.py
+++ Airfoil.py
@@ -1,25 +1,32 @@
 import numpy as np
+import math
 #MK this is to be updated as airfoil = airfoil (alpha, cl_data, cd_data, cm_data), typically at each 0.5deg
-#testingggg
 
 class Airfoil:
-    def _init_(self, cl_data: dict, cd_data: dict, cm_data: dict):
-        self.cl_data = cl_data
-        self.cd_data = cd_data
-        self.cm_data = cm_data
+    def __init__(self, alpha):
+        Cl=2*math.pi*alpha
+        return Cl
 
-    cl_data = {'alpha': [0, 5, 10, 15], 'Cl': [0.0, 0.5, 1.0, 0.8]}
-    cd_data = {'alpha': [0, 5, 10, 15], 'Cd': [0.01, 0.015, 0.02, 0.025]}
-    cm_data = {'alpha': [0, 5, 10, 15], 'Cm': [0.0, -0.02, -0.04, -0.03]}
+# DO NOT TOUCH, INITIAL SET OF DATA FOR THE FIRST ITERATION
+# 
+#  class Airfoil:
+#     def _init_(self, cl_data: dict, cd_data: dict, cm_data: dict):
+#         self.cl_data = cl_data
+#         self.cd_data = cd_data
+#         self.cm_data = cm_data
 
-    def get_cl(self, AOA: float):
-        return np.interp(AOA, self.cl_data['alpha'], self.cl_data['Cl'])
+#     cl_data = {'alpha': [0, 5, 10, 15], 'Cl': [0.0, 0.5, 1.0, 0.8]}
+#     cd_data = {'alpha': [0, 5, 10, 15], 'Cd': [0.01, 0.015, 0.02, 0.025]}
+#     cm_data = {'alpha': [0, 5, 10, 15], 'Cm': [0.0, -0.02, -0.04, -0.03]}
+
+#     def get_cl(self, AOA: float):
+#         return np.interp(AOA, self.cl_data['alpha'], self.cl_data['Cl'])
     
-    def get_cd(self, AOA: float):
-        return np.interp(AOA, self.cd_data['alpha'], self.cd_data['Cd'])
+#     def get_cd(self, AOA: float):
+#         return np.interp(AOA, self.cd_data['alpha'], self.cd_data['Cd'])
 
-    def get_cm(self, AOA: float):
-        return np.interp(AOA, self.cm_data['alpha'], self.cm_data['Cd'])
+#     def get_cm(self, AOA: float):
+#         return np.interp(AOA, self.cm_data['alpha'], self.cm_data['Cd'])
 
 
 

354c7499ee590861015686b809342c906231615a
Sauroncool <ambujnayan919@gmail.com>
2024-08-30 01:43:37 +0530

    Added

--- mission planner/mission-planner.py
+++ mission planner/mission-planner.py
@@ -1,8 +1,10 @@
 from mission_inputs import *
 
 Thrust = 2000 # N
-def fuel_burn_rate:
+def fuel_burn_rate(Thrust):
     m_dot = 0.03*Thrust
     return m_dot
 
-def fuel_weight
+def fuel_weight(w_0,t):
+    w_f = w_0 - fuel_burn_rate(Thrust) * t
+    return w_f

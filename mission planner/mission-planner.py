7403bab7b6d804aece69e3cd5b9963050bf58227
Your Name <Swarna Dutta>
2024-08-31 15:21:42 +0530

    updated mission planner

--- mission planner/mission-planner.py
+++ mission planner/mission-planner.py
@@ -1,15 +1,30 @@
 from mission_inputs import *
 
 Thrust = 2000 # N
+fuel_density=0.804
 def fuel_burn_rate(Thrust):
     m_dot = 0.03*Thrust
     return m_dot
 
 
-def update_weights(fuel_weight, m_dot):
+Power_required = 75         # P=T*omega, Pmr+Ptr, From Flight simulator
+Power_available = 93.43     # From the engine specifications
+SFC = 0.35                  # From engine specifications
+
+if Power_available>Power_required:
+    mf_dot=SFC*Power_required                              # The TSFC here will vary as per the engine specification
+    fuel_flow=mf_dot/fuel_density
+else:
+    print("Insufficient Power")
+    
+def update_weights(mf_dot):
     fuel_weight-=mf_dot*dt
     vehicle_weight-=min_dot+mf0-mf_dot*dt       # min_dot-->inert mass of helicopter, mf_dot=fuel burn weight
     return fuel_weight, vehicle_weight
+
+def feasibility_check():                        # accounts for insufficient engine power/ potential stall/ insufficient fuel 
+
+
 
 
 
--- mission planner/mission_inputs.py
+++ mission planner/mission_inputs.py
@@ -1,16 +1,16 @@
 tk_off_weight = 500 # kg
-tk_off_attitude = 400 # m
-fuel_weight = 500 # kg
+tk_off_altitude = 400 # m
+initial_fuel_weight = 500 # kg
 
-def MS_user_inputs():
-    user_inputs = {
-        'main_radius'       : float(main_radius_Entry.get()),
-        'main_number'       : int(main_number_Entry.get()),
-        'main_omega'        : float(main_omega_Entry.get()),
+# def MS_user_inputs():
+#     user_inputs = {
+#         'tk_off_weight'       : float(tk_off_wt_entry.get()),
+#         'tk_off_altitude'       : float(take_off_altitude_entry.get()),
+#         'initial_fuel_weight'        : float(fuel_weight_entry.get()),
       
-    }
-    print(user_inputs)  # For debugging
-    return user_inputs
+#     }
+#     print(user_inputs)  # For debugging
+#     return user_inputs
 
 #Hover and vertical climb
 # put params here

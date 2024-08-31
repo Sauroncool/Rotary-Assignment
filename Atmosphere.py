e0f0c4407b8571c628ef131013cf2b86dac274f7
Your Name <Swarna Dutta>
2024-08-31 17:03:00 +0530

    updates in MP

--- Atmosphere.py
+++ Atmosphere.py
@@ -8,9 +8,11 @@ class Atmosphere:
         self.pressure=101325
         self.density=1.225
 
-    def calc_conditions(self, altitude:float):
+    def calc_conditions(altitude:float):
         # Calculates variations in atmospheric properties with altitude
-        self.altitude = altitude
-        self.temperature = 288.15-0.0065*altitude
-        self.pressure = 101325*(1-0.0065*altitude/288.15)**5.2561
+        altitude = altitude
+        temperature = 288.15-0.0065*altitude
+        pressure = 101325*(1-0.0065*altitude/288.15)**5.2561
+        density = 101325*(1-0.0065*altitude/288.15)**4.2561
+        return temperature, pressure, density
         
--- mission planner/mission-planner.py
+++ mission planner/mission-planner.py
@@ -1,6 +1,47 @@
 from mission_inputs import *
+from CI import *
+from FS_user_inputs2 import *
+from Atmosphere import *
 
-Thrust = 2000 # N
+
+rho=Atmosphere.calc_conditions(tk_off_altitude).density
+AOA_stall=14
+Blade_Area=0.8
+initial_omega=user_inputs.main_omega
+theta=Cyclic_Integrator().theta
+
+def Lift_Calculator(Cl, velocity):
+    Lift=0.5*rho*Blade_velocity**2*Cl*Blade_Area
+    return Lift
+
+
+for omega in np.linspace(initial_omega, initial_omega+100, 50):
+    for theta in np.linspace(theta, theta+15, 15):
+        alpha=Cyclic_Integrator.AOA
+        if alpha==AOA_stall:
+            Cl=2*np.pi*alpha
+            Blade_velocity=omega*user_inputs.r
+            Lift=Lift_Calculator(Cl, Blade_velocity)
+            print(f"The maximum take off lift is {Lift}")
+            break
+
+
+# while alpha1<AOA_stall:
+#     alpha1=Cyclic_Integrator.AOA
+#     if alpha1==AOA_stall:
+#         break
+#     omega+=1
+#     while alpha2<AOA_stall:
+#         alpha2=Cyclic_Integrator.AOA
+#         if alpha2==AOA_stall:
+#             break
+#         twist+=1
+
+
+    
+
+
+# Thrust = 2000 # N
 Qf     = 43.124             #MJ/kg
 fuel_density=0.804          #kg/L
 
@@ -16,7 +57,7 @@ SFC = 0.35                  # From engine specifications
 
 if Power_available>Power_required:
     mf_dot=SFC*Power_required                              # The TSFC here will vary as per the engine specification
-    fuel_flow=mf_dot/fuel_density
+    # fuel_flow=mf_dot/fuel_density
 else:
     print("Insufficient Power")
 
